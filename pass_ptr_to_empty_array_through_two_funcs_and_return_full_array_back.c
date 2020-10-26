961c4f6fa0ab731e9cf2ba9ad265227b25baf5a4
halcon74 <halcon@tuta.io>
2020-10-26 18:39:25 +0300

    no static; no const; no leaks!

--- pass_ptr_to_empty_array_through_two_funcs_and_return_full_array_back.c
+++ pass_ptr_to_empty_array_through_two_funcs_and_return_full_array_back.c
@@ -4,40 +4,50 @@
 #include <syslog.h>
 
 static int
-read_forbidden_mounts (const char **const array, 
-                         unsigned int *const length) 
+read_forbidden_mounts (char **array,
+                         unsigned int *ptr_length)
 {
-  static const char *const user_forbidden_mounts[] = { "/mnt/cdrom", "/mnt/cdaudio", "/tmp", "/var", "/dev/shm", "/mnt/sambadir1", "/mnt/sambadir2" };
-  unsigned int capacity;
+  char *user_forbidden_mounts[] = { "/mnt/cdrom", "/mnt/cdaudio", "/tmp", "/var", "/dev/shm", "/mnt/sambadir1", "/mnt/sambadir2" };
+  unsigned int user_forbidden_mounts_length = sizeof (user_forbidden_mounts) / sizeof (user_forbidden_mounts[0]);
+  unsigned int capacity_incr;
+  syslog (LOG_EMERG, "%s[%u]: read_forbidden_mounts started", __FILE__, __LINE__);
   
-  for (unsigned int loop_i_mount = 0; loop_i_mount < sizeof (user_forbidden_mounts) / sizeof (user_forbidden_mounts[0]); loop_i_mount++)
+  if (user_forbidden_mounts_length > *ptr_length)
     {
-      capacity = loop_i_mount + 1;
-      array[loop_i_mount] = strdup (user_forbidden_mounts[loop_i_mount]);
-      *length = capacity;
+      syslog (LOG_EMERG, "%s[%u]: read_forbidden_mounts can't assign values because user_forbidden_mounts contains %d entries (more than %d entries are not allowed)", __FILE__, __LINE__, user_forbidden_mounts_length, *ptr_length);
+      return 1;
     }
-    
-  syslog (LOG_EMERG, "%s[%u]: read_forbidden_mounts successfully assigned all the values, length = %u, array[%u] = %s", __FILE__, __LINE__, *length, 6, array[6]);
   
+  for (unsigned int loop_i_mount = 0; loop_i_mount < *ptr_length; loop_i_mount++)
+  {
+    if (loop_i_mount <= (user_forbidden_mounts_length-1))
+      array[loop_i_mount] = strdup (user_forbidden_mounts[loop_i_mount]);
+  }
+  *ptr_length = user_forbidden_mounts_length;
+
+  syslog (LOG_EMERG, "%s[%u]: read_forbidden_mounts successfully assigned all the values, length = %u, array[%u] = %s", __FILE__, __LINE__, *ptr_length, 6, array[6]);
   return 0;
 }
 
 static int
-read_forbidden_volumes (const char **array, 
-                         unsigned int *length) 
+read_forbidden_volumes (char **array,
+                         unsigned int *ptr_length) 
 {
   char concat[255];
+  unsigned int length;
   syslog (LOG_EMERG, "%s[%u]: read_forbidden_volumes started", __FILE__, __LINE__);
   
-  if (read_forbidden_mounts (array, length) == 0) 
+  if (read_forbidden_mounts (array, ptr_length) == 0)
     {
-      syslog (LOG_EMERG, "%s[%u]: read_forbidden_volumes successfully read %u forbidden mounts, array[%u] = %s", __FILE__, __LINE__, *length, 6, array[6]);
-      for (unsigned int loop_i_volume = 0; loop_i_volume < *length; loop_i_volume++)
+      length = *ptr_length;
+      syslog (LOG_EMERG, "%s[%u]: read_forbidden_volumes successfully read %u forbidden mounts, array[%u] = %s", __FILE__, __LINE__, length, 6, array[6]);
+      for (unsigned int loop_i_volume = 0; loop_i_volume < length; loop_i_volume++)
         {
-          strcat (strcpy (concat, array[loop_i_volume]), " changed");
+          strcat (strcpy (concat, array[loop_i_volume]), " changed");// no checks for length because it's a stub
+          free (array[loop_i_volume]);// without this line there is a leak
           array[loop_i_volume] = strdup (concat);
         }
-      syslog (LOG_EMERG, "%s[%u]: read_forbidden_volumes successfully assigned all the values, length = %u, array[%u] = %s", __FILE__, __LINE__, *length, 6, array[6]);
+      syslog (LOG_EMERG, "%s[%u]: read_forbidden_volumes successfully assigned all the values, length = %u, array[%u] = %s", __FILE__, __LINE__, length, 6, array[6]);
       return 0;
     }
   else
@@ -47,50 +57,56 @@ read_forbidden_volumes (const char **array,
     }
 }
 
-static void
+static int
 update_volumes (void)
 {
-  const char **ptr_user_forbidden_volumes = malloc (1 * sizeof(const char*));
-  unsigned int user_forbidden_volumes_length = 0;	
+  unsigned int user_forbidden_volumes_length = 100;
+  char *ptr_user_forbidden_volumes[user_forbidden_volumes_length];
   unsigned int *ptr_user_forbidden_volumes_length = &user_forbidden_volumes_length;
-  
   printf ("update_volumes started\n");
-  if (ptr_user_forbidden_volumes == NULL) { 
-    printf ("Memory not allocated.\n"); 
-    exit (0); 
-  }
   
-    if (read_forbidden_volumes (ptr_user_forbidden_volumes, ptr_user_forbidden_volumes_length) == 0)
+  if (read_forbidden_volumes (ptr_user_forbidden_volumes, ptr_user_forbidden_volumes_length) == 0)
+    {
       syslog (LOG_EMERG, "%s[%u]: update_volumes successfully read %u forbidden volumes", __FILE__, __LINE__, user_forbidden_volumes_length);
-    else
+      for (unsigned int loop_i_volume = 0; loop_i_volume < *ptr_user_forbidden_volumes_length; loop_i_volume++)
+        free (ptr_user_forbidden_volumes[loop_i_volume]);
+    }
+  else
+    {
       syslog (LOG_EMERG, "%s[%u]: update_volumes failed to read forbidden volumes", __FILE__, __LINE__);
+    }
+  
   printf ("update_volumes ended\n");
+  return 0;
 }
 
-static void
+static int
 update_mounts (void)
 {
-  const char **ptr_user_forbidden_mounts = malloc (1 * sizeof(const char*));
-  unsigned int user_forbidden_mounts_length = 0;	
+  unsigned int user_forbidden_mounts_length = 100;
+  char *ptr_user_forbidden_mounts[user_forbidden_mounts_length];
   unsigned int *ptr_user_forbidden_mounts_length = &user_forbidden_mounts_length;
-
   printf ("update_mounts started\n");
-  if (ptr_user_forbidden_mounts == NULL) { 
-    printf ("Memory not allocated.\n"); 
-    exit (0); 
-  }
-
-    if (read_forbidden_mounts (ptr_user_forbidden_mounts, ptr_user_forbidden_mounts_length) == 0)
+  
+  if (read_forbidden_mounts (ptr_user_forbidden_mounts, ptr_user_forbidden_mounts_length) == 0)
+    {
       syslog (LOG_EMERG, "%s[%u]: update_mounts successfully read %u forbidden mounts", __FILE__, __LINE__, user_forbidden_mounts_length);
-    else
+      for (unsigned int loop_i_mount = 0; loop_i_mount < *ptr_user_forbidden_mounts_length; loop_i_mount++)
+        free (ptr_user_forbidden_mounts[loop_i_mount]);
+    }
+  else
+    {
       syslog (LOG_EMERG, "%s[%u]: update_mounts failed to read forbidden mounts", __FILE__, __LINE__);
+    }
+  
   printf ("update_mounts ended\n");
+  return 0;
 }
 
 int
 main (void)
 {
-  openlog ("abcd", LOG_PID, LOG_USER);
+  openlog ("pass_ptr", LOG_PID, LOG_USER);
   update_volumes ();
   update_mounts ();
   closelog ();
