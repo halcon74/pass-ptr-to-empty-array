f687a87b9fb8f8954423abce0f8748ae7c92fb2d
halcon74 <halcon@tuta.io>
2020-10-31 18:06:06 +0300

    remove a wrapper function read_forbidden_mounts (read_fstab is real read_forbidden_mounts)

--- pass_ptr_to_empty_array_through_two_funcs_and_return_full_array_back.c
+++ pass_ptr_to_empty_array_through_two_funcs_and_return_full_array_back.c
@@ -7,7 +7,7 @@
 #define MAX_USER_FORBIDDEN_MOUNTS_LENGTH 100
 
 static int
-read_fstab (char **array,
+read_forbidden_mounts (char **array,
                          unsigned int *ptr_length)
 {
 if (1 == 1) {// in glib: #ifdef HAVE_GETMNTENT_R
@@ -67,49 +67,6 @@ if (1 == 1) {// in glib: #ifdef HAVE_GETMNTENT_R
   syslog (LOG_EMERG, "%s[%u]: read_fstab can't find getmntent_r", __FILE__, __LINE__);
   return 1;
 }// in glib: #endif
-}
-
-static int
-read_forbidden_mounts (char **array,
-                         unsigned int *ptr_length)
-{
-  char *user_forbidden_mounts[MAX_USER_FORBIDDEN_MOUNTS_LENGTH];
-  unsigned int user_forbidden_mounts_length = sizeof(user_forbidden_mounts) / sizeof(user_forbidden_mounts[0]);
-  unsigned int *ptr_user_forbidden_mounts_length = &user_forbidden_mounts_length;
-  unsigned int last_el;// used in syslog only
-
-  syslog (LOG_EMERG, "%s[%u]: read_forbidden_mounts started", __FILE__, __LINE__);
-
-  if (read_fstab (user_forbidden_mounts, ptr_user_forbidden_mounts_length) != 0)
-    {
-      syslog (LOG_EMERG, "%s[%u]: read_forbidden_mounts can't continue", __FILE__, __LINE__);
-      return 1;
-    }
-  
-  if (user_forbidden_mounts_length > *ptr_length)
-    {
-      syslog (LOG_EMERG, "%s[%u]: read_forbidden_mounts can't assign values because "
-              "user_forbidden_mounts contains %d entries (more than %d entries are not allowed)", 
-              __FILE__, __LINE__, user_forbidden_mounts_length, *ptr_length);
-      return 1;
-    }
-  
-  for (unsigned int loop_i_mount = 0; loop_i_mount < user_forbidden_mounts_length; loop_i_mount++)
-    {
-      if ((array[loop_i_mount] = strdup (user_forbidden_mounts[loop_i_mount])) == NULL)
-        {
-          syslog (LOG_EMERG, "%s[%u]: read_forbidden_mounts failed to allocate memory", 
-                  __FILE__, __LINE__);
-          return 1;
-        }
-      free (user_forbidden_mounts[loop_i_mount]);
-    }
-  *ptr_length = user_forbidden_mounts_length;
-  last_el = user_forbidden_mounts_length - 1;
-
-  syslog (LOG_EMERG, "%s[%u]: read_forbidden_mounts successfully assigned all the values, "
-          "length = %u, array[%u] = %s", __FILE__, __LINE__, *ptr_length, last_el, array[last_el]);
-  return 0;
 }
 
 static int
