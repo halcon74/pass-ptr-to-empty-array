c07a6542afa6d9345a03998b097ecede1c7bcd0b
halcon74 <halcon@tuta.io>
2020-11-04 02:29:00 +0300

    fix (the second) freeing the failed element

--- pass_ptr_to_empty_array__file_messages.c
+++ pass_ptr_to_empty_array__file_messages.c
@@ -114,7 +114,7 @@ read_forbidden_volumes (char **array,
       
       if (failed_el_index != UINT_MAX)
         {
-          for (unsigned int loop_j = 0; loop_j <= failed_el_index; loop_j++)
+          for (unsigned int loop_j = 0; loop_j < failed_el_index; loop_j++)
             free (array[loop_j]);
           return 1;
         }
